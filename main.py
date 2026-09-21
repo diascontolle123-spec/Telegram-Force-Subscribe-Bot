663ed3c225569db293da35d0bf1a6fb86f51516f
diascontolle123-spec <diascontolle123@gmail.com>
2026-09-21 17:15:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ MENU_APK_SAMURAI = "⚔️ Link APK Samurai"
 MENU_TUTORIAL = "📖 Tutorial"
 APK_NINJA_URL = "https://sub4unlock.com/S/05mxk"
 APK_SAMURAI_URL = "https://sub4unlock.com/S/Z5olj"
-GET_KEY_URL = "-"
+GET_KEY_URL = "https://exe.io/GetkeyNinja-Samurai"
 STATUS_TEXT = "AKTIF"
 # Status Kunci Samurai (Default: Terkunci)
 SAMURAI_ACCESS_OPEN = False
@@ -52,8 +52,8 @@ TUTORIAL_URL = "https://youtu.be/94COnGxw15A?si=gVm0Qjos7Cfk_3Ao"
 ADMIN_USERNAME = "@ADAMYOURBAE"
 USERS_DB_PATH = os.getenv("USERS_DB_PATH", "data/users.db")
 DISPLAY_TIMEZONE = os.getenv("BOT_TIMEZONE", "Asia/Jakarta")
-MAX_DEVICE_TEXT = "-"
-MANUAL_EXPIRY_TEXT = "-"
+MAX_DEVICE_TEXT = "0/50"
+MANUAL_EXPIRY_TEXT = "22 September 17.30"
 KEEP_ALIVE_PORT = int(os.getenv("KEEP_ALIVE_PORT", os.getenv("PORT", "5000")))
 RECONNECT_DELAY_SECONDS = int(os.getenv("RECONNECT_DELAY_SECONDS", "5"))
 KEY_VALIDITY = timedelta(hours=24)
