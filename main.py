d6d3ca292c2da8bdc4811fd1b3518c0acacf69d1
diascontolle123-spec <diascontolle123@gmail.com>
2026-09-25 07:37:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ TUTORIAL_URL = "https://youtu.be/94COnGxw15A?si=gVm0Qjos7Cfk_3Ao"
 ADMIN_USERNAME = "@ADAMYOURBAE"
 USERS_DB_PATH = os.getenv("USERS_DB_PATH", "data/users.db")
 DISPLAY_TIMEZONE = os.getenv("BOT_TIMEZONE", "Asia/Jakarta")
-MAX_DEVICE_TEXT = "50/100 Devices"
+MAX_DEVICE_TEXT = "70/100 Devices"
 MANUAL_EXPIRY_TEXT = "25 September 20.30"
 KEEP_ALIVE_PORT = int(os.getenv("KEEP_ALIVE_PORT", os.getenv("PORT", "5000")))
 RECONNECT_DELAY_SECONDS = int(os.getenv("RECONNECT_DELAY_SECONDS", "5"))
